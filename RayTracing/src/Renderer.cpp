1e79eed76104faddbb76637fd0990e87d1595852
Yuvi <81169447+YUVRAJg37@users.noreply.github.com>
2022-12-16 20:08:46 +0530

    Lighting and Shading

--- RayTracing/src/Renderer.cpp
+++ RayTracing/src/Renderer.cpp
@@ -1,6 +1,21 @@
 #include "Renderer.h"
 #include "Walnut/Random.h"
 
+namespace utils
+{
+	static uint32_t ConvertToRGBA(const glm::vec4& color)
+	{
+		uint8_t red = (uint8_t)(color.r * 255.0f);
+		uint8_t green = (uint8_t)(color.g * 255.0f);
+		uint8_t blue = (uint8_t)(color.b * 255.0f);
+		uint8_t alpha = (uint8_t)(color.a * 255.0f);
+
+		uint32_t result = (alpha << 24) | (blue << 16) | (green << 8) | red;
+
+		return result;
+	}
+}
+
 void Renderer::OnResize(uint32_t width, uint32_t height)
 {
 	if(m_FinalImage)
@@ -26,29 +41,39 @@ void Renderer::Render()
 		{
 			glm::vec2 coord = { (float)x / (float)m_FinalImage->GetWidth(), (float)y / (float)m_FinalImage->GetHeight() };
 			coord = coord * 2.0f - 1.0f;
-			m_ImageData[x + y*m_FinalImage->GetWidth()] = PerPixel(coord);
+
+			glm::vec4 color = PerPixel(coord);
+			color = glm::clamp(color, glm::vec4(0.0f), glm::vec4(1.0f));
+			m_ImageData[x + y * m_FinalImage->GetWidth()] = utils::ConvertToRGBA(color);
 		}
 	}
 
 	m_FinalImage->SetData(m_ImageData);
 }
 
-uint32_t Renderer::PerPixel(glm::vec2 coord)
+glm::vec4 Renderer::PerPixel(glm::vec2 coord)
 {
-	uint8_t red = (uint8_t)(coord.x * 255.0f);
-	uint8_t green = (uint8_t)(coord.y * 255.0f);
-
-	glm::vec3 raydirection = { coord.x, coord.y, -1.0f };
-	glm::vec3 rayOrigin{ 0.0f, 0.0f, 2.0f };
+	glm::vec3 rayDirection = { coord.x, coord.y, -1.0f };
+	glm::vec3 rayOrigin{ 0.0f, 0.0f, 1.0f };
+	glm::vec3 lightDirection = glm::normalize(glm::vec3(-1, -1 ,-1));
 	float radius = 0.5f;
 
-	float a = glm::dot(raydirection, raydirection);
-	float b = 2*glm::dot(raydirection, rayOrigin);
+	float a = glm::dot(rayDirection, rayDirection);
+	float b = 2*glm::dot(rayDirection, rayOrigin);
 	float c = glm::dot(rayOrigin, rayOrigin) - radius * radius;
 
-	float d = b * b - 4.0f * a * c;
+	float discriminant = b * b - 4.0f * a * c;
 
-	if(d>=0)
-		return 0xffff00ff;
-	return 0xff000000;
+	if(discriminant<0.0f)
+		return glm::vec4(0,0,0,1);
+
+	float closestHit = (- b - glm::sqrt(discriminant)) / (2.0f * a);
+	glm::vec3 hitPoint = rayOrigin + rayDirection * closestHit;
+	glm::vec3 normal = glm::normalize(hitPoint);
+	float dot = glm::max(glm::dot(normal, -lightDirection), 0.0f);
+
+	glm::vec3 sphereColor(1, 0, 1);
+	sphereColor *= dot;
+
+	return glm::vec4(sphereColor, 1.0f);
 }
