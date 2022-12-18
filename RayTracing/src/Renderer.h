c24e24fb92f35f674de88b344bd00cba2c997923
Yuvi <81169447+YUVRAJg37@users.noreply.github.com>
2022-12-19 01:09:15 +0530

    Refactoring and Bounce Lighting

--- RayTracing/src/Renderer.h
+++ RayTracing/src/Renderer.h
@@ -4,21 +4,37 @@
 #include <memory>
 #include "Camera.h"
 #include "Ray.h"
+#include "Scene.h"
 
 class Renderer
 {
 public:
 
 	void OnResize(uint32_t width, uint32_t height);
-	void Render(const Camera& m_Camera);
+	void Render(const Scene& scene,const Camera& camera);
 
 	std::shared_ptr<Walnut::Image> GetFinalImage() { return m_FinalImage; }
 
 private:
+	struct HitPayload
+	{
+		float HitDistance;
+		glm::vec3 WorldPosition;
+		glm::vec3 WorldNormal;
 
-	glm::vec4 TraceRay(const Ray& ray);
+		int ObjectIndex;
+	};
+
+	glm::vec4 PerPixel(uint32_t x, uint32_t y);
+
+	HitPayload TraceRay(const Ray& ray);
+	HitPayload ClosestHit(const Ray& ray, float hitDistance, int objectIndex);
+	HitPayload MissRay(const Ray& ray);
 
 private:
+
+	const Scene* m_ActiveScene = nullptr;
+	const Camera* m_ActiveCamera = nullptr;
 
 	std::shared_ptr<Walnut::Image> m_FinalImage;
 	uint32_t* m_ImageData = nullptr;
--- RayTracing/src/WalnutApp.cpp
+++ RayTracing/src/WalnutApp.cpp
@@ -6,6 +6,7 @@
 #include "Walnut/Timer.h"
 #include "Renderer.h"
 #include "Camera.h"
+#include "glm/gtc/type_ptr.hpp"
 
 using namespace Walnut;
 
@@ -14,7 +15,16 @@ class ExampleLayer : public Walnut::Layer
 public:
 
 	ExampleLayer() :
-		m_Camera(45.0f, 0.1f, 100.0f) {}
+		m_Camera(45.0f, 0.1f, 100.0f)
+	{
+		
+		Sphere sphere;
+		sphere.Position = { 0.0f, 0.0f, 0.0f };
+		sphere.Radius = 0.5f;
+		sphere.Albedo = { 1.0f, 0.0f, 0.0f };
+		m_Scene.Spheres.push_back(sphere);
+		
+	}
 
 	void OnUpdate(float ts) override
 	{
@@ -29,6 +39,28 @@ public:
 		{
 			Render();
 		}
+		ImGui::End();
+
+		ImGui::Begin("Scene");
+
+		for (size_t i = 0; i < m_Scene.Spheres.size(); i++)
+		{
+			ImGui::PushID(i);
+
+			Sphere& sphere = m_Scene.Spheres[i];
+
+			ImGui::DragFloat3("Position", glm::value_ptr(sphere.Position), 0.1f);
+			ImGui::DragFloat("Radius", &sphere.Radius, 0.1f);
+			ImGui::ColorEdit3("Scale", glm::value_ptr(sphere.Albedo), 0.1f);
+
+			ImGui::Separator();
+
+			ImGui::PopID();
+		}
+
+		if (ImGui::Button("Add Sphere"))
+			AddSphere();
+
 		ImGui::End();
 
 		ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(0.0f, 0.0f));
@@ -54,17 +86,28 @@ public:
 
 		m_Renderer.OnResize(m_ViewportWidth, m_ViewportHeight);
 		m_Camera.OnResize(m_ViewportWidth, m_ViewportHeight);
-		m_Renderer.Render(m_Camera);
+		m_Renderer.Render(m_Scene, m_Camera);
 
 		m_LastRenderTime = timer.ElapsedMillis();
+	}
+
+	void AddSphere()
+	{
+		Sphere sphere;
+		sphere.Position = { 0.0f, 0.0f, 0.0f };
+		sphere.Radius = 0.5f;
+		sphere.Albedo = { 1.0f, 1.0f, 0.0f };
+		m_Scene.Spheres.push_back(sphere);
 	}
 
 private:
 
 	Renderer m_Renderer;
 	Camera m_Camera;
+	Scene m_Scene;
 	uint32_t m_ViewportWidth, m_ViewportHeight;
 	float m_LastRenderTime = 0.0f;
+
 };
 
 Walnut::Application* Walnut::CreateApplication(int argc, char** argv)
